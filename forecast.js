2347ef14daba5de75f7dd097ffce84269346b7c2
petr <petr@localhost>
2018-12-06 12:41:19 +0100

    change forecast page printing

--- forecast.js
+++ forecast.js
@@ -1,5 +1,5 @@
 "use strict";
-const main = document.querySelector('main');
+const main = document.querySelector('#page-content');
 const appKey = "b554424da5715ae40ee25d7ac9c307bf";
 const requestOpt = "&units=metric&lang=cz";
 const weatherUrl = "https://api.openweathermap.org/data/2.5/";
@@ -10,7 +10,7 @@ var weather;
 
 window.addEventListener("load", async e => {
 	await fetchWeather();
-	main.innerHTML = `Místo: ${weather.city.name} <br>`;
+	main.innerHTML = `<div class="card"><div class="card-header">${weather.city.name} </div> </div>`;
 	for (let i = 0; i < weather.list.length; i++) {
 		main.innerHTML += printWeather(weather.list[i]);
 	}
@@ -32,14 +32,32 @@ async function fetchWeather() {
 
 function printWeather(weather) {
 	return `
-		${weather.dt_txt} UTC <br>
-		Teplota: ${weather.main.temp} °C <br>
-		Počasí: ${weather.weather[0].description} <br>
-		Nejnižší teplota: ${weather.main.temp_min} °C <br>
-		Nejvyšší teplota: ${weather.main.temp_max} °C <br>
-		Vlhkost: ${weather.main.humidity} % <br>
-		Atmosférický tlak: ${weather.main.pressure} hPa <br>
-		<img src="${imgUrl}${weather.weather[0].icon}.png" alt="${weather.weather[0].description}" width="100" height="100"> <br>
+		<div class="data-table card" >
+			<div class="card-header">
+		${weather.dt_txt} UTC
+			</div>
+			<div class="card-content">
+				<table>
+					<tbody id="weatherDescription">
+		<tr>
+		<td>Teplota:</td><td> ${weather.main.temp} °C </td>
+		</tr><tr>
+		<td>Počasí:</td><td> ${weather.weather[0].description} </td>
+		</tr><tr>
+		<td>Nejnižší teplota:</td><td> ${weather.main.temp_min} °C </td>
+		</tr><tr>
+		<td>Nejvyšší teplota:</td><td> ${weather.main.temp_max} °C </td>
+		</tr><tr>
+		<td>Vlhkost:</td><td> ${weather.main.humidity} % </td>
+		</tr><tr>
+		<td>Atmosférický tlak:</td><td> ${weather.main.pressure} hPa </td>
+		</tr><tr>
+		<td colspan="2"><img src="${imgUrl}${weather.weather[0].icon}.png" alt="${weather.weather[0].description}" width="100" height="100"> </td>
+		</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
 		`;
 };
 
